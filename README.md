0fa241cd9a5725d9e249ec2a2807e751639c5e37
eltoman <eltomds@usp.br>
2020-10-01 19:50:42 -0300

    add tutorial 2 - twitter producer

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # udemy-apache-kafka
 studies about apache kafka and java
+
+
+### Tutorial2 - TwitterProducer
+Configure your secrets file  ~/.spring-boot-devtools.properties
+
+with keys:
+
+TWITTER_CONSUMER_KEY=
+TWITTER_CONSUMER_SECRET=
+TWITTER_TOKEN=
+TWITTER_SECRET=
--- kafka-producer-twitter/src/main/java/com/studies/kafka/tutorial2/Application.java
+++ kafka-producer-twitter/src/main/java/com/studies/kafka/tutorial2/Application.java
@@ -1,4 +1,17 @@
 package com.studies.kafka.tutorial2;
 
-public class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        context.getBean(TwitterProducer.class).run();
+    }
 }
+
+
+
